08adc93e7ae84ce21c17dbb2ef7a00c34f6d5c6b
Villaedi <ediberto.sanchez.villalobos@gmail.com>
2023-12-10 22:45:59 -0600

    Events,commands and install MongoDB

---
+++ Events/Client/ready.js
@@ -0,0 +1,24 @@
+const {client} = require('discord.js');
+const mongoose = require('mongoose');
+//const mongoose = require('mongodb');
+const config = require('../../config.json');
+require('colors')
+module.exports = {
+    name:"ready",
+    once:true,
+    /**
+     * 
+     * @param {Client} client 
+     */
+    async execute(client){
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(config.databaseURL,{
+            keepAlive:true,
+        }).then(() => {
+            console.log('[MONGODB]conect DATABASE :D'.green);
+        }).catch((err) =>{
+            console.log(`Could not connect to the database: ${err}`.red)
+        })
+        console.log(`El ${client.user.username} is online!`)
+    }
+};
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ChatLogicMaster
+
+
+install in the project
+    Command: npm i ascii-table fs colors
+    - npm colors
+    - npm ascii-table
+    - nap fs
+    
