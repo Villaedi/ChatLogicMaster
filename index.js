85efb114a100f2d6389c2cfb868e604b2a8bddf3
Villaedi <ediberto.sanchez.villalobos@gmail.com>
2023-12-15 18:55:56 -0600

    Fuga

--- index.js
+++ index.js
@@ -22,3 +22,27 @@ client.login(config.token).then(async() => {
 }).catch((err) =>{
     console.log(err)
 })
+
+//aqui
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apikey: [config.OPENAI_API_KEY],
+});
+
+async function main() {
+    try {
+      const completion = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: "dame un numero aleatorio. ",
+        max_tokens: 7,
+        temperature: 0,
+      });
+  
+      console.log(completion);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  main();
